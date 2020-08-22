607d20002bcb580e7752477b59632ec28b6d5ac8
Lenovo <Lenovo@DESKTOP-G5JUJVB>
2020-08-22 21:58:37 +0500

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,50 @@
+require("./bootstrap");
+window.Vue = require("vue");
+
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+import VueAxios from "vue-axios";
+import axios from "axios";
+
+import App from "./App.vue";
+Vue.use(VueAxios, axios);
+
+import HomeComponent from "./components/HomeComponent.vue";
+import CreateComponent from "./components/CreateComponent.vue";
+import IndexComponent from "./components/IndexComponent.vue";
+import EditComponent from "./components/EditComponent.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+const routes = [
+    {
+        name: "home",
+        path: "/",
+        component: HomeComponent
+    },
+    {
+        name: "create",
+        path: "/create",
+        component: CreateComponent
+    },
+    {
+        name: "posts",
+        path: "/posts",
+        component: IndexComponent
+    },
+    {
+        name: "edit",
+        path: "/edit/:id",
+        component: EditComponent
+    }
+];
+
+const router = new VueRouter({ mode: "history", routes: routes });
+const app = new Vue(Vue.util.extend({ router }, App)).$mount("#app");
---
+++ resources/js/application.js
@@ -0,0 +1,3 @@
+function apiUrl() {
+    return "http://127.0.0.1:8000/";
+}
